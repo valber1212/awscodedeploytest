8a4ec1a885e40da990fe39480eb6eb112fd4b2eb
Valber Carvalho <61290728+valber1212@users.noreply.github.com>
2023-08-20 09:42:44 -0300

    Coping the entire app

---
+++ AppTestCodeDeploy/scripts/ApplicationStop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# stop http service
+
+#sudo http service
+sudo systemctl stop httpd.service
---
+++ AppTestCodeDeploy/scripts/BeforeInstall.sh
@@ -0,0 +1,5 @@
+# delete all content of folder html
+rm -rf /var/www/html
+
+# update the system
+sudo yum -y update
